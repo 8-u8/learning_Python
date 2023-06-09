768f6aad4051347d465642c36aadf86ee70c0b68
8-u8 <itotaka523@gmail.com>
2023-06-09 18:01:41 +0900

    constarints modified

--- ortool_trial/ouj_report_program.py
+++ ortool_trial/ouj_report_program.py
@@ -16,6 +16,11 @@ def create_data_model():
         [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0], # const3
         [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0], # const4
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], # const5
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0], # const6
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], # const7
+        [0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0], # const8
+        [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0], # const9
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1]  # const10
     ]
     # the bounds of constraints.
     data['bounds'] = [1,1,1,1,1]
@@ -31,8 +36,12 @@ data = create_data_model()
 solver = pywraplp.Solver.CreateSolver('SCIP')
 
 x = {}
+name_list = ['x11', 'x13', 'x22', 'x24', 'x25', 'x31', 'x33', 'x34', 
+             'x43', 'x45', 'x51', 'x52', 'x55']
 for i in range(data['num_vars']):
     x[i] = solver.IntVar(0, 1, 'x[%i]' % i)
+    tmp_keys = list(x.keys())[i]
+    x[name_list[i]] = x.pop(tmp_keys)
 print('Number of variables = ', solver.NumVariables())
 
 # %% define constraints 
@@ -40,23 +49,27 @@ print('Number of variables = ', solver.NumVariables())
 for i in range(data['num_constraints']):
     constraint = solver.RowConstraint(0, data['bounds'][i], '')
     for j in range(data['num_vars']):
-        constraint.SetCoefficient(x[j], data['constraint_coeffs'][i][j])
+        tmp_idx = list(x.keys())[j]
+        constraint.SetCoefficient(x[tmp_idx], data['constraint_coeffs'][i][j])
 print('Number of constraints = ', solver.NumConstraints())
 
 # %% define objectives
 
 objective = solver.Objective()
 for j in range(data['num_vars']):
-    objective.SetCoefficient(x[j], data['obj_coeffs'][j])
+    tmp_idx = list(x.keys())[j]
+    objective.SetCoefficient(x[tmp_idx], data['obj_coeffs'][j])
 
 objective.SetMaximization()
 # %%
 status = solver.Solve()
 # %%
 if status == pywraplp.Solver.OPTIMAL:
+    
     print('Objective value =', solver.Objective().Value())
-    for j in range(data['num_vars']):
-        print(x[j].name(), ' = ', x[j].solution_value())
+    for j in name_list:
+        tmp_idx = j
+        print(j, ' = ', x[j].solution_value())
     print()
     print('Problem solved in %f milliseconds' % solver.wall_time())
     print('Problem solved in %d iterations' % solver.iterations())
